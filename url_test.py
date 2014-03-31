0fa546b25aaa3f5cb0f3eba1ac8aac1a67453cd0
xlin <xlin@memphis.edu>
2014-03-31 02:06:40 -0500

    remove an error in url_test

--- url_test.py
+++ url_test.py
@@ -3,6 +3,6 @@ import cyan
 import views
 
 if __name__=='__main__':
-    cyan.route('hello_cyan', hello_cyan)
-    cyan.route('seeyou_cyan', seeyou_cyan)
+    cyan.route('hello_cyan', views.hello_cyan)
+    cyan.route('seeyou_cyan', views.seeyou_cyan)
     cyan.runapp('localhost', 20143, views.devcyan)
